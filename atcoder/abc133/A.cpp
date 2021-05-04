e20314eb7dba8a16f109754fb5dd68ce2e715f84
Nabil0day <hadiurrahmannabil@gmail.com>
2021-05-04 22:16:00 +0600

    Add solution for problem `A - T or T` Link: https://atcoder.jp/contests/abc133/tasks/abc133_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc133/submissions/22313643

---
+++ atcoder/abc133/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+ using namespace std;
+ 
+ int main() {
+ int N, A, B;
+   
+   cin >> N;
+   cin >> A;
+   cin >> B;
+   
+  int cost=min(N*A,B);
+   cout<< cost <<endl;
+   
+ }
